7100fa66487be9daea5035a40f7405aeef714c56
Joshua Shelto <joshuashelto@gmail.com>
2019-04-01 23:48:22 -0400

    Update social buttons

--- src/QuoteBox.js
+++ src/QuoteBox.js
@@ -4,32 +4,33 @@ import { FaTumblrSquare, FaTwitterSquare,FaQuoteLeft } from 'react-icons/fa';
 import { IconContext } from 'react-icons';
 
 const QuoteText = ({quote}) => {
-  return <div id='text' className='quote-text'><FaQuoteLeft />{`${quote}`}</div>
+  return <div id='text' className='quote-text'><FaQuoteLeft />{` ${quote}`}</div>
 };
 
 const AuthorText = ({author}) => {
   return <div id='author' className='quote-author'>{`- ${author}`}</div>
 };
 
-const RetweetButton = ({quote,color}) => {
+const RetweetButton = ({value}) => {
   return (
-    <a href={`https://twitter.com/intent/tweet?text=${quote}`} id='tweet-quote' target='_blank' rel='noopener noreferrer'>
-      <IconContext.Provider value={{ color: color, className: 'social-button', size: '1.5em' }}>
+    <a href={`https://twitter.com/intent/tweet?text=${value.quote}`} id='tweet-quote' target='_blank' rel='noopener noreferrer'>
+      <IconContext.Provider value={{ color: value.color, className: 'social-button', size: '1.5em' }}>
         <FaTwitterSquare />
       </IconContext.Provider>
     </a>
   )
 }
 
-const TumblrButton = ({quote,color}) => {
+const TumblrButton = ({value}) => {
   return (
-    <a href={`https://www.tumblr.com/widgets/share/tool?posttype=quote&caption=${quote}`} id='tumble-quote' target='_blank' rel='noopener noreferrer'>
-      <IconContext.Provider value={{ color: color, className: 'social-button', size: '1.5em' }}>
+    <a href={`https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption==${value.author}&content=${value.quote}&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button`} id='tumble-quote' target='_blank' rel='noopener noreferrer'>
+      <IconContext.Provider value={{ color: value.color, className: 'social-button', size: '1.5em' }}>
         <FaTumblrSquare />
       </IconContext.Provider>
     </a>
   )
 }
+
 
 export default class QuoteBox extends Component {
   render() {
@@ -43,8 +44,8 @@ export default class QuoteBox extends Component {
                 <AuthorText author={value.author}/>
                 <div className='button-container'>
                   <div className='social-button-container'>
-                    <RetweetButton color={value.color}/>
-                    <TumblrButton color={value.color}/>
+                    <RetweetButton value={value}/>
+                    <TumblrButton value={value}/>
                   </div>
                   <button id='new-quote' className='button' onClick={()=>{value.fetchQuote()}}>New Quote</button>
                 </div>
